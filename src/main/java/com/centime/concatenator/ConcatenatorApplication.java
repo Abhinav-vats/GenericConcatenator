1ea7ddad4bb86d90b3286765cc258f76a4eaaf67
Abhinav-vats <50848068+Abhinav-vats@users.noreply.github.com>
2025-07-09 22:35:59 +0530

    First Commit

---
+++ src/main/java/com/centime/concatenator/ConcatenatorApplication.java
@@ -0,0 +1,13 @@
+package com.centime.concatenator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcatenatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcatenatorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/centime/concatenator/controller/GeneralConcatenatorController.java
@@ -0,0 +1,12 @@
+package com.centime.concatenator.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/general")
+@Slf4j
+public class GeneralConcatenatorController {
+}
