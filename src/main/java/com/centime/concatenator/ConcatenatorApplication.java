a47f1ba09db7d3fbcd2fff55895b3d5904cc3289
Abhinav-vats <50848068+Abhinav-vats@users.noreply.github.com>
2025-07-11 20:08:30 +0530

    swagger ui added

--- src/main/java/com/centime/concatenator/ConcatenatorApplication.java
+++ src/main/java/com/centime/concatenator/ConcatenatorApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableSwagger2
 public class ConcatenatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConcatenatorApplication.class, args);
 	}
+
 
 }
